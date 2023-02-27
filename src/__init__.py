2155b7e210327cee180c0b3463b2c5d4a1cbca37
dark <mynamedark713@gmail.com>
2023-02-27 22:30:20 +0200

    add config

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask 
+from src import config
 
 
 def create_app():
 
 	app = Flask(__name__)
+	app.config.from_object(config)
 
 	@app.route("/")
 	def index():
@@ -11,7 +13,7 @@ def create_app():
 
 	from src.auth.routes import auth_bp
 	from src.search.routes import search_bp
-	
+
 	app.register_blueprint(auth_bp, url_prefix="/auth")
 	app.register_blueprint(search_bp, url_prefix="/search")
 
