0590af5ffc6507ab11ba6c6ac98d3be33e6ad634
dark <mynamedark713@gmail.com>
2023-02-28 16:26:32 +0200

    add register back

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, flash, redirect, url_for
+from flask import Blueprint, render_template, flash, redirect, url_for, current_app
+
+from src import db
+from src.auth import auth_service
 from .forms import RegisterForm, LoginForm
 
 
@@ -9,7 +12,12 @@ auth_bp = Blueprint("auth_bp", template_folder="templates", static_folder="stati
 def login():
 	form = LoginForm()
 	if form.validate_on_submit():
-		print(2)
+		user = auth_service.find_user_by_email(db.session, form.email.data)
+		if user and user.check_pasword(form.password):
+			pass
+			# log in user
+		else:
+			flash("Wrong email or password")
 
 	return render_template("login.html", form=form)
 
@@ -18,8 +26,11 @@ def login():
 def register():
 	form = RegisterForm()
 	if form.validate_on_submit():
-		print(1)
-		return redirect(url_for(".login"))
+		user = auth_service.create_user(db.session, form.email.data, form.password.data)
+		if not user:
+			flash("User with this email already exist")
+		else:
+			return redirect(url_for(".login"))
 	return render_template("register.html", form=form)
 
 
