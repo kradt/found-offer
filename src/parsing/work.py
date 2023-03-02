acfb66b2d70c794696fcf3d9a0162c564e2246b1
dark <mynamedark713@gmail.com>
2023-03-02 22:49:57 +0200

    start writing work ua class for parsing

--- src/parsing/work.py
+++ src/parsing/work.py
@@ -1,1 +1,33 @@
-from bs4 import BeautifulSoup 
+from requests_html import HTMLSession
+
+
+class WorkUA:
+	__link = "https://www.work.ua/{}"
+
+	def __init__(self):
+		self.session = HTMLSession()
+
+
+	def _create_link_by_filters(self,
+								 city: str | None = None,
+								 job: str | None = None, 
+								 category: list[int] | None = None, 
+								 type_of_employ: list[int] | None = None, 
+								 salary: tuple[int, int] | None = None) -> str:
+		link = self.__link
+		job_block = "jobs"
+		if city:
+			job_block += f"-{city}"
+		if job:
+			job_block += f"-{job}"
+
+		return link.format(job_block)
+
+
+	
+
+
+
+work = WorkUA()
+soup = work._create_link_by_filters("kiev", "backend")
+print(soup)
