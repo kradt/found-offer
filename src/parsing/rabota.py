89f43e82f895415f60c653f8ed804e45403d643e
dark <mynamedark713@gmail.com>
2023-03-11 13:58:58 +0200

    add models

---
+++ src/parsing/rabota.py
@@ -0,0 +1,9 @@
+
+
+
+class RabotaUA:
+	__url = "https://rabota.ua/ua/{}"
+	__per_page = 
+	def __init__(self, *args, **kwargs):
+		super().__init__(self, *args, **kwargs)
+		self.session = HTMLSession()
