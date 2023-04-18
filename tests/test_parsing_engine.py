1e681d5b57ddefc1f384ab5253ec0555e62d83e4
krad <mynamedark713@gmail.com>
2023-04-18 23:40:14 +0300

    refactor test

--- tests/test_parsing_engine.py
+++ tests/test_parsing_engine.py
@@ -7,7 +7,10 @@ from src.parsing import engines as core
 def test_class_JobsUA_iterable():
     engine = core.JobsUA()
     offers = next(engine)
-    print(offers)
+    assert engine.current_page == 1
     for offer in offers:
         assert offer.title
+        assert offer.city
         assert isinstance(offer.title, str)
+        assert isinstance(offer.city, str)
+        assert isinstance(offer, core.OfferModel)
