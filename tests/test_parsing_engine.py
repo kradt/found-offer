04efd3e81d9f5faca47b4b6b6ae595ad5ac7d733
krad <mynamedark713@gmail.com>
2023-04-18 23:42:30 +0300

    add assert current-page

--- tests/test_parsing_engine.py
+++ tests/test_parsing_engine.py
@@ -14,3 +14,6 @@ def test_class_JobsUA_iterable():
         assert isinstance(offer.title, str)
         assert isinstance(offer.city, str)
         assert isinstance(offer, core.OfferModel)
+
+    next(engine)
+    assert engine.current_page == 2
