afaaeca830c7e94119784449d72f58c15252e805
krad <mynamedark713@gmail.com>
2023-04-29 18:12:32 +0300

    refactor salary filters

--- src/search/routes.py
+++ src/search/routes.py
@@ -3,31 +3,32 @@ from src.database import models
 from .forms import FilterForm
 from ..parsing import engines
 
-
 search_bp = Blueprint("search_bp", template_folder="templates", import_name=__name__)
 
 
 @search_bp.route("/", methods=["GET", "POST"])
 def find_work():
-	form = FilterForm()
-	vacancies = models.Vacancy.objects()[:50]
-	if form.validate_on_submit():
-		filter_dict = {}
-		if form.title:
-			filter_dict["title__icontains"] = form.title.data
-		elif form.city:
-			filter_dict["city__icontains"] = form.city.data
-		elif form.salary_from:
-			filter_dict["salary_from__gte"] = form.salary_from.data
-		elif form.salary_to:
-			filter_dict["salary_to__lte"] = form.salary_to.data
-		vacancies = models.Vacancy.objects(**filter_dict)[:50]
+    form = FilterForm()
+    vacancies = models.Vacancy.objects()[:15]
+    if form.validate_on_submit():
+        filter_dict = {}
+        print(dir(form))
 
+        if form.title.data:
+            filter_dict["title__icontains"] = form.title.data
+        if form.city.data:
+            filter_dict["city__icontains"] = form.city.data
+        if form.salary_from.data:
+            filter_dict["salary_from__gte"] = form.salary_from.data
+        if form.salary_to.data:
+            filter_dict["salary_to__lte"] = form.salary_to.data
+        vacancies = models.Vacancy.objects(**filter_dict)[:20]
+        print(vacancies)
 
-	return render_template("search.html", form=form, vacancies=vacancies)
+    return render_template("search.html", form=form, vacancies=vacancies)
 
 
 @search_bp.route("/offers/<job>", methods=["GET"])
 def gtf(job):
-	query = engines.WorkUA().get_page(job=job)
-	return str(query.paginate(10, 1))
+    query = engines.WorkUA().get_page(job=job)
+    return str(query.paginate(10, 1))
