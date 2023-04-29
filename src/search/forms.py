83a019ecc91c308f41ceee92514b2dd04bbcc60f
krad <mynamedark713@gmail.com>
2023-04-29 17:48:44 +0300

    add backend to filters

--- src/search/forms.py
+++ src/search/forms.py
@@ -8,3 +8,4 @@ class FilterForm(FlaskForm):
     city = StringField("City", render_kw={"placeholder": "Enter Job location"})
     salary_from = StringField("Salary From", render_kw={"placeholder": "From"})
     salary_to = StringField("Salary To", render_kw={"placeholder": "To"})
+    submit = SubmitField("Search")
--- src/search/routes.py
+++ src/search/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, jsonify
-
+from src.database import models
 from .forms import FilterForm
 from ..parsing import engines
 
@@ -10,10 +10,21 @@ search_bp = Blueprint("search_bp", template_folder="templates", import_name=__na
 @search_bp.route("/", methods=["GET", "POST"])
 def find_work():
 	form = FilterForm()
+	vacancies = models.Vacancy.objects()[:50]
 	if form.validate_on_submit():
-		pass
+		filter_dict = {}
+		if form.title:
+			filter_dict["title__icontains"] = form.title.data
+		elif form.city:
+			filter_dict["city__icontains"] = form.city.data
+		elif form.salary_from:
+			filter_dict["salary_from__gte"] = form.salary_from.data
+		elif form.salary_to:
+			filter_dict["salary_to__lte"] = form.salary_to.data
+		vacancies = models.Vacancy.objects(**filter_dict)[:50]
 
-	return render_template("search.html", form=form)
+
+	return render_template("search.html", form=form, vacancies=vacancies)
 
 
 @search_bp.route("/offers/<job>", methods=["GET"])
