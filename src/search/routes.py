04e6e4f36a1a32ee36e33d60770bc72be6fb8594
krad <mynamedark713@gmail.com>
2023-04-30 18:59:00 +0300

    add sorting value

--- src/search/routes.py
+++ src/search/routes.py
@@ -12,24 +12,27 @@ search_bp = Blueprint("search_bp", template_folder="templates", import_name=__na
 @search_bp.route("/", methods=["GET"])
 def find_work():
     form = FilterForm(request.args, meta={'csrf': False})
-    if form.validate():
-        filter_dict = {}
-        if form.title.data:
-            filter_dict["title__icontains"] = form.title.data
-        if form.city.data:
-            filter_dict["city__icontains"] = form.city.data
-        if form.salary_from.data:
-            filter_dict["salary_from__gte"] = form.salary_from.data
-        if form.salary_to.data:
-            filter_dict["salary_to__lte"] = form.salary_to.data
-        vacancies = models.Vacancy.objects(**filter_dict)
-        if not vacancies:
-            flash("Вибачте, але по вашому запиту ще немає вакансій")
+
+    filter_dict = {}
+    if form.title.data:
+        filter_dict["title__icontains"] = form.title.data
+    if form.city.data:
+        filter_dict["city__icontains"] = form.city.data
+    if form.salary_from.data:
+        filter_dict["salary_from__gte"] = form.salary_from.data
+    if form.salary_to.data:
+        filter_dict["salary_to__lte"] = form.salary_to.data
+    sort_value = "-time_publish"
+    if request.args.get("sort_by") == '2':
+        sort_value = "-salary_from"
+
+    vacancies = models.Vacancy.objects(**filter_dict).order_by(sort_value)
+    if not vacancies:
+        flash("Вибачте, але по вашому запиту ще немає вакансій")
     current_page = int(request.args.get('page', 1))
 
     items_per_page = 20
     vacancies = vacancies.paginate(page=current_page, per_page=items_per_page)
-
     return render_template(
         "search.html",
         form=form,
