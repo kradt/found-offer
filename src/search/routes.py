76e9df5e98830b0ce1a63d7fd98bc4d0f3ee6a48
krad <mynamedark713@gmail.com>
2023-04-30 11:46:52 +0300

    add parse args

--- src/search/routes.py
+++ src/search/routes.py
@@ -1,7 +1,7 @@
 import datetime
 import math
 
-from flask import Blueprint, render_template, jsonify, flash, request
+from flask import Blueprint, render_template, jsonify, flash, request, Response
 from src.database import models
 from .forms import FilterForm
 from ..parsing import engines
@@ -9,25 +9,23 @@ from ..parsing import engines
 search_bp = Blueprint("search_bp", template_folder="templates", import_name=__name__)
 
 
-@search_bp.route("/", methods=["GET", "POST"])
+@search_bp.route("/", methods=["GET"])
 def find_work():
-    form = FilterForm()
-    if form.validate_on_submit():
-        filter_dict = {}
+    form = FilterForm(request.args, meta={'csrf': False})
+    parsed_request_args = "&".join(f"{arg}={request.args.get(arg)}" for arg in request.args)
+    filter_dict = {}
+    if form.title.data:
+        filter_dict["title__icontains"] = form.title.data
+    if form.city.data:
+        filter_dict["city__icontains"] = form.city.data
+    if form.salary_from.data:
+        filter_dict["salary_from__gte"] = form.salary_from.data
+    if form.salary_to.data:
+        filter_dict["salary_to__lte"] = form.salary_to.data
+    vacancies = models.Vacancy.objects(**filter_dict)
+    if not vacancies:
+        flash("Вибачте, але по вашому запиту ще немає вакансій")
 
-        if form.title.data:
-            filter_dict["title__icontains"] = form.title.data
-        if form.city.data:
-            filter_dict["city__icontains"] = form.city.data
-        if form.salary_from.data:
-            filter_dict["salary_from__gte"] = form.salary_from.data
-        if form.salary_to.data:
-            filter_dict["salary_to__lte"] = form.salary_to.data
-        vacancies = models.Vacancy.objects(**filter_dict)
-        if not vacancies:
-            flash("Вибачте, але по вашому запиту ще немає вакансій")
-    else:
-        vacancies = models.Vacancy.objects()
     current_page = int(request.args.get("page", 1))
     items_per_page = 20
     offset = (current_page - 1) * items_per_page
