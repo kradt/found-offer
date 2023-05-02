2d8f48157709dc7a3d4dd95bbac4853e82b8d676
krad <mynamedark713@gmail.com>
2023-05-02 22:22:01 +0300

    celery first run

--- src/auth/tasks.py
+++ src/auth/tasks.py
@@ -2,6 +2,7 @@ from celery import shared_task
 from flask_mail import Message
 from src import mail
 
+
 @shared_task()
 def send_message_to_email(send_data: dict):
 	msg = Message(
--- src/celery_utils.py
+++ src/celery_utils.py
@@ -10,5 +10,7 @@ def celery_init_app(app: Flask) -> Celery:
 
     celery_app = Celery(app.name, task_cls=FlaskTask)
     celery_app.config_from_object(app.config["CELERY"])
+    celery_app.set_default()
+    app.extensions["celery"] = celery_app
     return celery_app
 
